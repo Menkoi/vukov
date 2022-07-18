153a9c5611a8ef5f3e6bb75c8e085159aaf1510a
celaeon <celaeon@gmail.com>
2022-07-18 17:07:10 +0200

    added react/three, skybox

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,22 @@
+import React, {Suspense} from 'react';
+import {Sky, PerspectiveCamera, Stage} from '@react-three/drei';
+import {Canvas} from '@react-three/fiber';
+
+
 function Home() {
     return (
-        <h1> Lorem Ipsum </h1>
+        <div className='Main'>
+            <Canvas>
+                <Suspense fallback={null}>
+
+                    <PerspectiveCamera/>
+                    <Sky/>
+                    <Stage/>
+
+                </Suspense>
+            </Canvas>
+        </div>
+        
     )
 }
 
