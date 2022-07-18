13498ed3e4c902487aa40d464d0f51b600c7e141
celaeon <celaeon@gmail.com>
2022-07-18 18:39:13 +0200

    add default box and free camera movement

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,44 @@
 import React, {Suspense} from 'react';
-import {Sky, PerspectiveCamera, Stage} from '@react-three/drei';
-import {Canvas} from '@react-three/fiber';
+import {Sky, OrbitControls, PerspectiveCamera, Html} from '@react-three/drei';
+import {Canvas, useLoader} from '@react-three/fiber';
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
 
+import Default from './img/default.png';
+
+// Create main menu JS
+// Create seperate file for box 
+// Add floor
 
 function Home() {
+    const textureDef = useLoader(TextureLoader, Default)
     return (
         <div className='Main'>
             <Canvas>
+            <ambientLight intensity={1} />
+                
+                {/* Need to add loading screen here */}
                 <Suspense fallback={null}>
 
-                    <PerspectiveCamera/>
+                    {/* Camera */}
+                    <PerspectiveCamera makeDefault position={[0, 1, 5]}/>
+                    <OrbitControls/>
+
+                    {/* SkyBox */}
                     <Sky/>
-                    <Stage/>
+
+                    <Html> 
+                        <h1>VUKOV</h1> 
+                    </Html>
+                    
+                    {/* Default Box */}
+                    <mesh>
+                        <boxBufferGeometry/>
+                        <meshStandardMaterial map={textureDef}/>
+                    </mesh>
 
                 </Suspense>
             </Canvas>
-        </div>
-        
+        </div>  
     )
 }
 
